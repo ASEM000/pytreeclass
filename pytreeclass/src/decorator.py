618d5929ce00a9e8a67d7889fd14e2887a1469d1
ASEM000 <mahmoudasem00@gmail.com>
2022-07-13 04:58:48 +0900

    add @run_once

--- pytreeclass/src/decorator.py
+++ pytreeclass/src/decorator.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import functools
 import inspect
+import warnings
 from dataclasses import dataclass, field
 
 import jax
@@ -38,3 +39,35 @@ def treeclass(*args, **kwargs):
     elif len(args) == 0 and len(kwargs) > 0:
         op = kwargs["op"] if "op" in kwargs else False
         return functools.partial(wrapper, op=op)
+
+
+def run_once(*args, **kwargs):
+
+    def run_once_wrapper(func, raise_error=True):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if not wrapper.run:
+                result = func(*args, **kwargs)
+                wrapper.run = True
+                return result
+            else:
+                if raise_error:
+                    raise ValueError(
+                        f"Function {func!r} is wrapped with @run_once is called more than once."
+                    )
+                else:
+                    warnings.warn(
+                        f"Function {func!r} wrapped with @run_once is called more than once."
+                    )
+
+        wrapper.run = False
+        return wrapper
+
+    if len(args) > 0 and inspect.isfunction(args[0]):
+        return run_once_wrapper(args[0], True)
+
+    elif len(args) == 0 and len(kwargs) > 0:
+        raise_error = kwargs[
+            "raise_error"] if "raise_error" in kwargs else False
+        return functools.partial(run_once_wrapper, raise_error=raise_error)
--- tests/test_tree_viz.py
+++ tests/test_tree_viz.py
@@ -3,7 +3,9 @@ from pytreeclass import tree_viz
 
 def test_vbox():
 
-    assert tree_viz.vbox('a',' a','a ') == '┌──┐\n│a │\n├──┤\n│ a│\n├──┤\n│a │\n└──┘'
+    assert tree_viz.vbox('a', ' a',
+                         'a ') == '┌──┐\n│a │\n├──┤\n│ a│\n├──┤\n│a │\n└──┘'
+
 
 def test_hbox():
-    assert tree_viz.hbox('a','b','c')  == '┌─┬─┬─┐\n│a│b│c│\n└─┴─┴─┘\n'
+    assert tree_viz.hbox('a', 'b', 'c') == '┌─┬─┬─┐\n│a│b│c│\n└─┴─┴─┘\n'
