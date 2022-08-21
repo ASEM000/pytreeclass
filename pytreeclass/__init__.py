89bef4fffa70b87d79eb44c8684d45f4d00ab66b
ASEM000 <mahmoudasem00@gmail.com>
2022-08-21 11:23:05 +0900

    Update __init__.py

--- pytreeclass/__init__.py
+++ pytreeclass/__init__.py
@@ -1,13 +1,14 @@
 from .src import tree_util, tree_viz
 from .src.decorator import treeclass
-from .src.tree_util import static_field
+from .src.tree_util import static_field, static_value
 
 __all__ = (
     "tree_viz",
     "treeclass",
+    "static_value",
     "static_field",
     "tree_viz",
     "tree_util",
 )
 
-__version__ = "0.0.9post0"
+__version__ = "0.0.9post1"
