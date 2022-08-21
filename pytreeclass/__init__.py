2458a1d7642128fb8a6b636a0a1f94fdafa6ceea
ASEM000 <mahmoudasem00@gmail.com>
2022-08-21 11:20:17 +0900

    bump version

--- pytreeclass/__init__.py
+++ pytreeclass/__init__.py
@@ -1,14 +1,13 @@
 from .src import tree_util, tree_viz
 from .src.decorator import treeclass
-from .src.tree_util import static_field, static_value
+from .src.tree_util import static_field
 
 __all__ = (
     "tree_viz",
     "treeclass",
-    "static_value",
     "static_field",
     "tree_viz",
     "tree_util",
 )
 
-__version__ = "0.0.9"
+__version__ = "0.0.9post0"
